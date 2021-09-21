c38611d332e2bb7ec7e5c660cd06630df53a58f8
Nicolasecm <nicolasecm@hotmail.com>
2021-09-20 22:06:54 -0500

    Se agrego bootstrap, header y footer, alertas.

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem 'ransack', github: 'activerecord-hackery/ransack'
+gem 'jquery-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+global.jQuery = require('jquery');
+require ('bootstrap');
+require ('@fortawesome/fontawesome-free/js/all');
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '@popperjs/core'
+
+$('document').ready(function() {
+    setTimeout(function() {
+      $('#flash').slideUp();
+    }, 5000);
+  });
